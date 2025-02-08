15c32ab0e42b6a47ffd7158171ec59fe5fa821d5
radeuoj <stefan.radu.arghira@gmail.com>
2025-02-08 15:18:44 +0200

    cleaned up renderer

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -7,7 +7,6 @@ void Renderer::init()
 
     glGenBuffers(1, &m_VBO);
     glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
-    // glBufferData(GL_ARRAY_BUFFER, this->vertices.size(), this->vertices.data(), GL_STATIC_DRAW);
 
     glVertexAttribIPointer(0, 2, GL_SHORT, sizeof(VertexData), (void*) offsetof(VertexData, chunk_x));
     glEnableVertexAttribArray(0);
@@ -18,102 +17,9 @@ void Renderer::init()
     glVertexAttribIPointer(2, 1, GL_UNSIGNED_BYTE, sizeof(VertexData), (void*) offsetof(VertexData, texCoords));
     glEnableVertexAttribArray(2);
 
-    // glVertexAttribIPointer (0, 3, GL_INT, sizeof(VertexData), (void*) offsetof(VertexData, x));
-    // glEnableVertexAttribArray(0);
-    //
-    // glVertexAttribIPointer(1, 2, GL_UNSIGNED_BYTE, sizeof(VertexData), (void*) offsetof(VertexData, texCoords));
-    // glEnableVertexAttribArray(1);
-
     // Unbinding
     glBindVertexArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
-}
-
-void Renderer::renderVertices(VertexData vertices[], GLsizeiptr size)
-{
-    //TODO: fix / remove
-
-    this->vertices.insert(this->vertices.end(), vertices, vertices + size);
-}
-
-void Renderer::renderSpriteVertices(VertexData vertices[])
-{
-    //TODO: fix / remove
-
-    // this->spriteVertices.insert(this->spriteVertices.end(), vertices, vertices + 30);
-    // this->spriteTextures.push_back(&texture);
-}
-
-void Renderer::renderSprite(glm::vec3 position, glm::vec2 size, Texture& texture)
-{
-    //TODO: fix / remove
-
-    // VertexData vertices[] =
-    // { /*                    COORDS                      //  TEXTURE COORDS */
-    //     position.x,          position.y,          position.z, 0.0f, 0.0f,
-    //     position.x + size.x, position.y,          position.z, 1.0f, 0.0f,
-    //     position.x + size.x, position.y + size.y, position.z, 1.0f, 1.0f,
-    //     position.x,          position.y,          position.z, 0.0f, 0.0f,
-    //     position.x + size.x, position.y + size.y, position.z, 1.0f, 1.0f,
-    //     position.x,          position.y + size.y, position.z, 0.0f, 1.0f,
-    // };
-    //
-    // this->RenderSpriteVertices(vertices);
-}
-
-void Renderer::renderBlock(glm::vec3 position, glm::vec3 scale, Texture& texture)
-{
-    // GLfloat vertices[] =
-    // { /*                    COORDS                                   //  TEXTURE COORDS */
-    //     position.x,           position.y,           position.z,           0.0f, 0.0f,
-    //     position.x + scale.x, position.y,           position.z,           1.0f, 0.0f,
-    //     position.x + scale.x, position.y + scale.y, position.z,           1.0f, 1.0f,
-    //     position.x,           position.y,           position.z,           0.0f, 0.0f,
-    //     position.x + scale.x, position.y + scale.y, position.z,           1.0f, 1.0f,
-    //     position.x,           position.y + scale.y, position.z,           0.0f, 1.0f,
-    //
-    //     position.x,           position.y,           position.z,           0.0f, 0.0f,
-    //     position.x + scale.x, position.y,           position.z,           1.0f, 0.0f,
-    //     position.x + scale.x, position.y,           position.z + scale.z, 1.0f, 1.0f,
-    //     position.x,           position.y,           position.z,           0.0f, 0.0f,
-    //     position.x + scale.x, position.y,           position.z + scale.z, 1.0f, 1.0f,
-    //     position.x,           position.y,           position.z + scale.z, 0.0f, 1.0f,
-    //
-    //     position.x,           position.y,           position.z + scale.z, 0.0f, 0.0f,
-    //     position.x + scale.x, position.y,           position.z + scale.z, 1.0f, 0.0f,
-    //     position.x + scale.x, position.y + scale.y, position.z + scale.z, 1.0f, 1.0f,
-    //     position.x,           position.y,           position.z + scale.z, 0.0f, 0.0f,
-    //     position.x + scale.x, position.y + scale.y, position.z + scale.z, 1.0f, 1.0f,
-    //     position.x,           position.y + scale.y, position.z + scale.z, 0.0f, 1.0f,
-    //
-    //     position.x,           position.y + scale.y, position.z,           0.0f, 0.0f,
-    //     position.x + scale.x, position.y + scale.y, position.z,           1.0f, 0.0f,
-    //     position.x + scale.x, position.y + scale.y, position.z + scale.z, 1.0f, 1.0f,
-    //     position.x,           position.y + scale.y, position.z,           0.0f, 0.0f,
-    //     position.x + scale.x, position.y + scale.y, position.z + scale.z, 1.0f, 1.0f,
-    //     position.x,           position.y + scale.y, position.z + scale.z, 0.0f, 1.0f,
-    //
-    //     position.x,           position.y,           position.z,           0.0f, 0.0f,
-    //     position.x,           position.y,           position.z + scale.z, 1.0f, 0.0f,
-    //     position.x,           position.y + scale.y, position.z + scale.z, 1.0f, 1.0f,
-    //     position.x,           position.y,           position.z,           0.0f, 0.0f,
-    //     position.x,           position.y + scale.y, position.z + scale.z, 1.0f, 1.0f,
-    //     position.x,           position.y + scale.y, position.z,           0.0f, 1.0f,
-    //
-    //     position.x + scale.x, position.y,           position.z,           1.0f, 0.0f,
-    //     position.x + scale.x, position.y,           position.z + scale.z, 0.0f, 0.0f,
-    //     position.x + scale.x, position.y + scale.y, position.z + scale.z, 0.0f, 1.0f,
-    //     position.x + scale.x, position.y,           position.z,           1.0f, 0.0f,
-    //     position.x + scale.x, position.y + scale.y, position.z + scale.z, 0.0f, 1.0f,
-    //     position.x + scale.x, position.y + scale.y, position.z,           1.0f, 1.0f,
-    // };
-    //
-    // this->spriteVertices.insert(this->spriteVertices.end(), vertices, vertices + std::size(vertices));
-
-    // for (int i = 0; i < 6; i++)
-    // {s
-    //     this->RenderSpriteVertices(vertices + (i * 30), texture);
-    // }
 }
 
 void Renderer::flush()
@@ -122,43 +28,14 @@ void Renderer::flush()
     glBindVertexArray(m_VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
-    glBufferData(GL_ARRAY_BUFFER, this->vertices.size() * sizeof(VertexData), vertices.data(), GL_DYNAMIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(VertexData), vertices.data(), GL_STATIC_DRAW);
 
-    // spriteTextures[0]->Bind();
-    glDrawArrays(GL_TRIANGLES, 0, this->vertices.size() * 5);
-
-    // for (int i = 0; i < this->spriteTextures.size(); i++)
-    // {
-    //     spriteTextures[i]->Bind();
-    //
-    //     glDrawArrays(GL_TRIANGLES, i * 30, (i + 1) * 30);
-    //
-    //     spriteTextures[i]->Unbind();
-    // }
+    glDrawArrays(GL_TRIANGLES, 0, vertices.size());
 
     glBindVertexArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-    // printf("%llu vs %llu\n", this->spriteVertices.capacity(), this->spriteVertices.size());
-
-    this->vertices.clear();
-    // this->spriteTextures.clear();
-}
-
-void Renderer::renderTriangleStrip(GLfloat vertices[], GLsizeiptr size)
-{
-    //TODO: fix / remove
-
-    // glBindVertexArray(VAO);
-    //
-    // glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    // glBufferData(GL_ARRAY_BUFFER, size * sizeof(GLfloat), vertices, GL_STATIC_DRAW);
-    //
-    // glDrawArrays(GL_TRIANGLE_STRIP, 0, size);
-    // // glDrawArrays(GL_LINE, 0, size);
-    //
-    // glBindVertexArray(0);
-    // glBindBuffer(GL_ARRAY_BUFFER, 0);
+    vertices.clear();
 }
 
 Renderer::~Renderer()
--- src/Renderer.h
+++ src/Renderer.h
@@ -18,25 +18,6 @@ public:
 
     ~Renderer();
 
-    /**
-     * !!!!!!!!!!!!!!!!!!!!!!DO NOT USE!!!!!!!!!!!!
-     * @deprecated dont use
-     */
-    void renderVertices(VertexData vertices[], GLsizeiptr size);
-
-    void renderTriangleStrip(GLfloat vertices[], GLsizeiptr size);
-
-    /**
-     *
-     * @param vertices MUST HAVE 4 VERTICES OF SIZE 5 GLfloats: 3 for coords, 2 for texcoords
-     * @param texture the texture
-     */
-    void renderSpriteVertices(VertexData vertices[]);
-
-    void renderSprite(glm::vec3 position, glm::vec2 size, Texture& texture);
-
-    void renderBlock(glm::vec3 position, glm::vec3 scale, Texture& texture);
-
     void flush();
 
     std::vector<VertexData> vertices;
